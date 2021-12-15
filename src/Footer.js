6d07596cb078910635e8499b6ee9a5712acbed57
BernadetteCh <b.christiner93@gmail.com>
2021-12-15 10:12:17 +0100

    created content for "Footer component" (Link to github, 3d Vectors)

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Footer() {
-  return <p>Hallo from Footer</p>;
+  return (
+    <div className="footer">
+      <a
+        href="https://github.com/BernadetteCh?tab=repositories"
+        target="_blank"
+        rel="noreferrer"
+        alt="github-link"
+      >
+        Open-source Code
+      </a>{" "}
+      by Bernadette Christiner
+      <p>
+        <a href="https://de.vecteezy.com/gratis-vektor/papier">
+          3d Vectors by Vecteezy
+        </a>
+      </p>
+    </div>
+  );
 }
