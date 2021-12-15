b1dc63dcc67e9f2acd2ea58364a49b349a6e2dea
BernadetteCh <b.christiner93@gmail.com>
2021-12-15 11:43:42 +0100

    created Forecast.js component and added forecast content with fake data

---
+++ src/Forecast.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Forecast.css";
+
+export default function Forecast() {
+  return (
+    <div className="weather_forecast">
+      <div className="row">
+        <div className="col-sm d-flex justify-content-center">
+          <div className="forecast">
+            <div className="forecast_first">
+              Thursday
+              <div className="weather-forecast-date"></div>
+              <img src="suncloud.png" className="forecast-icon" alt="" />
+              <div className="weather-forecast-temperature">5°</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm d-flex justify-content-center">
+          <div className="forecast">
+            <div className="forecast_first">
+              Thursday
+              <div className="weather-forecast-date"></div>
+              <img src="suncloud.png" className="forecast-icon" alt="" />
+              <div className="weather-forecast-temperature">5°</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm d-flex justify-content-center">
+          <div className="forecast">
+            <div className="forecast_first">
+              Thursday
+              <div className="weather-forecast-date"></div>
+              <img src="suncloud.png" className="forecast-icon" alt="" />
+              <div className="weather-forecast-temperature">5°</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm d-flex justify-content-center">
+          <div className="forecast">
+            <div className="forecast_first">
+              Thursday
+              <div className="weather-forecast-date"></div>
+              <img src="suncloud.png" className="forecast-icon" alt="" />
+              <div className="weather-forecast-temperature">5°</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm d-flex justify-content-center">
+          <div className="forecast">
+            <div className="forecast_first">
+              Thursday
+              <div className="weather-forecast-date"></div>
+              <img src="suncloud.png" className="forecast-icon" alt="" />
+              <div className="weather-forecast-temperature">5°</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
