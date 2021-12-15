25f4292bac105cf48c78b6aeb2f3860da56bfdf8
BernadetteCh <b.christiner93@gmail.com>
2021-12-15 11:51:52 +0100

    added link to netlify

--- src/Footer.js
+++ src/Footer.js
@@ -10,9 +10,10 @@ export default function Footer() {
         rel="noreferrer"
         alt="github-link"
       >
-        Open-source Code
+        Open-source Code,
       </a>{" "}
-      by Bernadette Christiner
+      by Bernadette Christiner and hosted on{" "}
+      <a href="https://optimistic-curran-8bd91f.netlify.app">Netlify</a>{" "}
       <p>
         <a href="https://de.vecteezy.com/gratis-vektor/papier">
           3d Vectors by Vecteezy
