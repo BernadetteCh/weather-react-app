8a5a017885ac690829919cf2f3c7cd9540f19f57
BernadetteCh <b.christiner93@gmail.com>
2021-12-20 21:55:18 +0100

    imported axios, made search form work with api (searching weather for any city)

--- src/Weather.js
+++ src/Weather.js
@@ -1,72 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-
 export default function Weather() {
-  let weatherData = {
-    city: "Graz",
-    temperature: 6,
-    description: "cloudy",
-    date: "Saturday 10:20",
-    humidity: 87,
-    wind: 6,
-    img: "suncloud.png",
-  };
-  return (
-    <div className="weatherinformation">
-      <div className="row justify-content-evenly mt-5">
-        <div className="col-4 ms-sm-5">
-          <section className="temperature">
-            <div className="celsius">
-              <span className="degree">{weatherData.temperature}</span>
-              <span className="units">
-                <a
-                  href="https://www.google.at/"
-                  className="celsius-temperature"
-                >
-                  °C
-                </a>
-              </span>
-            </div>
-          </section>
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [load, setLoad] = useState(null);
 
-          <img
-            src={weatherData.img}
-            className="icon"
-            alt={weatherData.description}
-          />
-          <h1>{weatherData.city}</h1>
-          <h2 id="date">{weatherData.date}</h2>
-        </div>
-        <div className="col-sm-6">
-          <section className="weather_data">
-            <li>
-              Description:{" "}
-              <span className="description">{weatherData.description}</span>
-            </li>
-            <li>
-              Humidity:
-              <span className="humidity">{weatherData.humidity}</span>%
-            </li>
-            <li>
-              Wind: <span className="wind">{weatherData.wind}</span>km/h
-            </li>
-          </section>
-        </div>
-      </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleApi();
+  }
 
-      <div className="search">
-        <form className="search-form">
-          <input
-            className="input-city"
-            type="text"
-            placeholder="Enter City"
-            autoComplete="off"
-          />
-          <input className="input-submit" type="submit" value="Search" />
-          <button>Current Location</button>
-        </form>
-      </div>
-    </div>
+  function handleChange(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function displayWeatherdata(response) {
+    setLoad(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+    });
+  }
+
+  function handleApi() {
+    let apikey = "4ccd9ecf2f417deee06840bdb3b5e20a";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
+    console.log(url);
+    axios.get(url).then(displayWeatherdata);
+  }
+
+  let form = (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <input
+        className="input-city"
+        onChange={handleChange}
+        type="text"
+        placeholder="Enter City"
+        autoComplete="off"
+      />
+      <input className="input-submit" type="submit" value="Search" />
+      <button>Current Location</button>
+    </form>
   );
+
+  if (load) {
+    return (
+      <div className="weatherinformation">
+        <div className="row justify-content-evenly mt-5">
+          <div className="col-4 ms-sm-5">
+            <section className="temperature">
+              <div className="celsius">
+                <span className="degree">{weather.temperature}</span>
+                <span className="units">
+                  <a
+                    href="https://www.google.at/"
+                    className="celsius-temperature"
+                  >
+                    °C
+                  </a>
+                </span>
+              </div>
+            </section>
+
+            <img src={weather.icon} className="icon" alt="description" />
+            <h1>{city}</h1>
+            <h2 id="date">Saturday 10:20</h2>
+          </div>
+          <div className="col-sm-6">
+            <section className="weather_data">
+              <li>
+                Description:{" "}
+                <span className="description">{weather.description}</span>
+              </li>
+              <li>
+                Humidity:
+                <span className="humidity">{weather.humidity}</span>%
+              </li>
+              <li>
+                Wind: <span className="wind">{weather.wind}</span>km/h
+              </li>
+            </section>
+          </div>
+        </div>
+
+        <div className="search">{form}</div>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
