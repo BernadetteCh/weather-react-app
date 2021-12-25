26f945d5024a58a163d75ae0652ac6b028cefdd7
BernadetteCh <b.christiner93@gmail.com>
2021-12-25 19:18:40 +0100

    imported new component WeatherInfo, made SearchEngine work(created new state, added EventListneners and created new functions)

--- src/Weather.js
+++ src/Weather.js
@@ -2,31 +2,48 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import "./FormattedDate";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo"; //für eine bessere Übersicht hob i die Daten (aktuelle Temperatur, Niederschlag etc.) die im return gerendert werden in des component hingesendet.
 
 export default function Weather(props) {
   const [weather, setWeather] = useState({ loaded: false }); //bei default hat es das value loaded:false
+  const [city, setCity] = useState(props.defaultCity);
 
   function displayWeatherdata(response) {
-    console.log(response.data);
+    console.log(response);
     setWeather({
       loaded: true,
       temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-      date: new Date(response.data.dt * 1000), //mit response.data.dt bekomm i die Computerzeit, die muss man mit 1000 multiplizieren und so kraig i die aktuelle Zeit. dann erstell i a component im return , dem diese Berechnung zugewiesen wird und des passiert mit FormattedDate date={weather.date}; newDate des is afoch der Code um zur aktuellen Zeit zu kommen, denn wenn i console.log(response.data.dt) moch erhalt i für dt eine längere Zahl. und mit newDate weiß der PC dass er die aktuelle Zeit ausrechnen muss, weil der PC bzw. JAva rechnet mit Zahlen seid ca 1970.....  
+      citysearch: response.data.name,
+      date: new Date(response.data.dt * 1000), //mit response.data.dt bekomm i die Computerzeit, die muss man mit 1000 multiplizieren und so kraig i die aktuelle Zeit. dann erstell i a component im return , dem diese Berechnung zugewiesen wird und des passiert mit FormattedDate date={weather.date}; newDate des is afoch der Code um zur aktuellen Zeit zu kommen, denn wenn i console.log(response.data.dt) moch erhalt i für dt eine längere Zahl. und mit newDate weiß der PC dass er die aktuelle Zeit ausrechnen muss, weil der PC bzw. JAva rechnet mit Zahlen seid ca 1970.....
       icon: ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+  function search() {
+    let apikey = "4ccd9ecf2f417deee06840bdb3b5e20a";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
+    axios.get(url).then(displayWeatherdata);
+    //im APi Url is city der Wert props.defaultCity gegben, damit beim Laden wenn ma noch keiner city sucht schon eine bei default quasi angezeigt wird
+  }
+
   let form = (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit}>
       <input
         className="input-city"
-        type="text"
-        placeholder="Enter City"
-        autoComplete="off"
+        type="search"
+        placeholder="Enter a City"
+        onChange={handleChange}
       />
       <input className="input-submit" type="submit" value="Search" />
       <button>Current Location</button>
@@ -36,51 +53,12 @@ export default function Weather(props) {
   if (weather.loaded) {
     return (
       <div className="weatherinformation">
-        <div className="row justify-content-evenly mt-5">
-          <div className="col-4 ms-sm-5">
-            <section className="temperature">
-              <div className="celsius">
-                <span className="degree">{weather.temperature}</span>
-                <span className="units">
-                  <a
-                    href="https://www.google.at/"
-                    className="celsius-temperature"
-                  >
-                    °C
-                  </a>
-                </span>
-              </div>
-            </section>
-
-            <img src={weather.icon} className="icon" alt="description" />
-            <h1>Paris</h1>
-            <h2 id="date">
-              <FormattedDate date={weather.date} />
-            </h2>
-          </div>
-          <div className="col-sm-6">
-            <section className="weather_data">
-              <li>
-                Description:{" "}
-                <span className="description">{weather.description}</span>
-              </li>
-              <li>
-                Humidity:
-                <span className="humidity">{weather.humidity}</span>%
-              </li>
-              <li>
-                Wind: <span className="wind">{weather.wind}</span>km/h
-              </li>
-            </section>
-          </div>
-        </div>
+        <WeatherInfo data={weather} />
         <div className="search">{form}</div>
       </div>
     );
   } else {
-    let apikey = "4ccd9ecf2f417deee06840bdb3b5e20a";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${apikey}`;
-    axios.get(url).then(displayWeatherdata);
+    search();
     return "Loading...";
   }
 }
