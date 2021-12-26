1cdae62b801f11d60c2c340b8fe398e83228a8d5
BernadetteCh <b.christiner93@gmail.com>
2021-12-26 11:42:57 +0100

    corrected if statement of minutes and hours

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -15,15 +15,15 @@ export default function FormattedDate(props) {
   let minutes = props.date.getMinutes(); //mit props.date hab i den Wert von response.data.dt*1000 also die aktuelle Zeit in dieses component geholt und mit getMinutes() holt der PC sich nur die Minuten; des gleiche gilt für getDay und getHous;
   if (minutes < 10) {
     //also anstatt 5:00 soll 05:00 angezeigt werden
-    minutes = `0:${minutes}`;
-  }
+    minutes = `0${minutes}`;}
+  
   let hours = props.date.getHours();
   if (hours < 10) {
-    hours = `0:${hours}`;
+    hours = `0${hours}`;
   }
   return (
     <div>
       {day} {hours}:{minutes}
     </div>
   );
-}
+  }
