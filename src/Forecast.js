5da02f4d2bade767996bbea24369296441500ea6
BernadetteCh <b.christiner93@gmail.com>
2021-12-29 19:33:50 +0100

    integrated apiUrl with coordinates for a weather forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Forecast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
-export default function Forecast() {
+export default function Forecast(props) {
+
+function handleForecast(response){
+  console.log(response);
+}
+
+let apikey = "4ccd9ecf2f417deee06840bdb3b5e20a";
+let lat=props.coords.lat;
+let lon=props.coords.lon;
+let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}`;
+axios.get(apiUrl).then(handleForecast);
+
+
+
   return (
     <div className="weather_forecast">
       <div className="row">
