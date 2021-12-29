9626b2dbe1e4f2ded82ea53af41ad4d8ba6897d0
BernadetteCh <b.christiner93@gmail.com>
2021-12-29 19:32:59 +0100

    removed components and class of conatiner_hero for styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
-import Hero from "./Hero";
+
 import Footer from "./Footer";
-import Forecast from "./Forecast";
+
+
 
 function App(props) {
   return (
     <div className="container">
       <div className="App">
-        <div className="container_hero">
-          <Hero />
+        
           <Weather defaultCity="Paris" />
-        </div>
-       <Forecast weatherinformation={props.weather}/>
         <Footer />
       </div>
     </div>
