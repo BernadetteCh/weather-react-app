cf8635f987a14bac649860d3aaab16a3c949a154
BernadetteCh <b.christiner93@gmail.com>
2022-01-01 13:47:22 +0100

    created a loop to loop through each day, and created useEffect to display the forecast when you search for a different city

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import ForecastDay from "./ForecastDay";
 import axios from "axios";
@@ -7,15 +7,36 @@ export default function Forecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
 
+ useEffect(()=>{
+   setLoaded(false);
+ }, [props.coords]);
+
   function handleForecast(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+  //hier einen loop erstellen damit jeder Tag angezeigt wird
   if (loaded) {
-    return <ForecastDay forecastday={forecast} />; //erstelle neues property wos die Daten im response.data.daily also a im forecast&(setForecast) san in des neue component für besseren Überblick
+    return (
+      <div className="weather_forecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if(index<5){
+              return (
+                <div
+                  className="col-sm d-flex justify-content-center"
+                  key={index}
+                >
+                  <ForecastDay forecastday={dailyForecast} />
+                </div>
+              );}
+          })}
+        </div>
+      </div>
+    ); //erstelle neues property wos die Daten im response.data.daily also a im forecast&(setForecast) san in des neue component für besseren Überblick
   } else {
-    let apikey = "4ccd9ecf2f417deee06840bdb3b5e20a";
+    let apikey = "fb5a52a1b1d04da9188f79aaf5843917";
     let lat = props.coords.lat;
     let lon = props.coords.lon;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
