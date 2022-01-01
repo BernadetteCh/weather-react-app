54e9765f7b5317bd9c274474d35513ad3b45f710
BernadetteCh <b.christiner93@gmail.com>
2022-01-01 17:14:05 +0100

    deleted Hero component, because the component had no purpose anymore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
-
 import Footer from "./Footer";
 
 
@@ -10,8 +9,7 @@ function App(props) {
   return (
     <div className="container">
       <div className="App">
-        
-          <Weather defaultCity="Paris" />
+        <Weather defaultCity="Paris" />
         <Footer />
       </div>
     </div>
