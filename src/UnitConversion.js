22b8a30c8087de33c10f44f31a7cf6c3e3bc683f
BernadetteCh <b.christiner93@gmail.com>
2022-01-15 17:15:28 +0100

    removed conversion to fahrenheit

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -1,41 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 export default function UnitConversion(props) {
-  const [unit, setUnit] = useState("celsius");
 
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
 
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
- 
-  }
+return(
+  <div className="celsius">
+        <span className="degree">{props.celsius}</span>
+        <span className="units">
+          <span className="celsius-temperature">°C </span>
+        </span>
+      </div>
+    );
 
+  /*
   if (unit === "celsius") {
     return (
       <div className="celsius">
         <span className="degree">{props.celsius}</span>
         <span className="units">
           <span className="celsius-temperature">°C | </span>
-          <a
-            href="/"
-            className="fahrenheit-temperature"
-            onClick={showFahrenheit}
-          >
-            F
-          </a>
         </span>
       </div>
     );
   } else {
-      let fahrenheit = Math.round( (props.celsius * 9) / 5 + 32);
     return (
       <div className="celsius">
-        <span className="degree">{fahrenheit}</span>
         <span className="units">
           <a href="/" className="celsius-temperature" onClick={showCelsius}>
             °C
@@ -44,5 +34,5 @@ export default function UnitConversion(props) {
         </span>
       </div>
     );
-  }
+  }*/
 }
